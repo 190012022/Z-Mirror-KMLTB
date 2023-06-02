6824997a0b283b677dd15c085342f73870c52dbb
190012022 <135198197+190012022@users.noreply.github.com>
2023-06-02 13:37:10 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -121,11 +121,11 @@ async def start(_, message):
         return await sendMessage(message, msg)
     elif config_dict['DM_MODE']:
         start_string = 'Bot Started.\n' \
-                       'Now I can send your stuff here.\n' \
-                       'Use me here: @Z_Mirror'
+                       'Now I can send your stuff here\n' \
+                       'Use me here @Kuromey'
     else:
         start_string = 'Sorry, you cant use me here!\n' \
-                       'Join @Z_Mirror to use me.\n' \
+                       'contact @Kuromey to use me\n' \
                        'Thank You'
     await sendMessage(message, start_string)
 
