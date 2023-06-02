e78cecb93c163f15c58052ded7af473823e4b0a5
190012022 <135198197+190012022@users.noreply.github.com>
2023-06-02 05:32:07 -0800

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -187,7 +187,7 @@ def get_readable_message():
             msg += f"\n» <b>Task</b> » <a href='{download.message.link}'>{download.extra_details['mode']}</a>"
 
         msg += f" | <b>By</b> » {tag}"
-        msg += f"\n⚠️ <code>/{BotCommands.CancelMirror[0]} {download.gid()}</code>\n\n"
+        msg += f"\n» <b>Cancel</b> » <code>/{BotCommands.CancelMirror[0]} {download.gid()}</code>\n\n"
 
     if len(msg) == 0:
         return None, None
